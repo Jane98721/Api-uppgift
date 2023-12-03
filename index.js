6d2b5c56a9c08f949139a6ed67b341372c8ac168
Jane98721 <144322813+Jane98721@users.noreply.github.com>
2023-12-03 20:14:02 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -22,31 +22,15 @@ const COLUMNS = ["id", "username", "password", "name", "email"];
 
 //Hämta alla
 app.get("/users", function (req, res) {
-    let sql = "SELECT * FROM users"; //kod
-    let condition = createCondition(req.query);//kod
-    console.log(sql + condition); //kod
+    let sql = "SELECT * FROM users";
+    let condition = createCondition(req.query);
+    console.log(sql + condition); 
     con.query(sql + condition, function (err, result, fields) {
       if (err) throw err
       res.json(result);
     });
   });
 
-// ID
- app.get("/users/:id", function (req, res) {
-    let sql = "SELECT * FROM users WHERE id=" + req.params.id;
-    console.log(sql);
-    
-    con.query(sql, function (err, result, fields) {
-      if (result.length > 0) {
-        res.send(result);
-      } else {
-        res.send("The column with the given ID was not found"); 
-      }
-    });
-  });
-
-
-//LÄS MER OM DETTA
   let createCondition = function (query) {
     console.log(query);
     let output = " WHERE ";
@@ -63,4 +47,64 @@ app.get("/users", function (req, res) {
   };
 
 
- 
+// ID
+ app.get("/users/:id", function (req, res) {
+    let sql = "SELECT * FROM users WHERE id=" + req.params.id;
+    console.log(sql);
+    
+    con.query(sql, function (err, result, fields) {
+      if (result.length > 0) {
+        res.send(result);
+      } else {
+        res.send("The column with the given ID was not found"); 
+      }
+    });
+  });
+
+  con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "jensen2023",
+  });
+
+
+
+
+//POST
+app.use(express.json());
+
+app.post('/users', function(req, res) {
+
+  if (!req.body.username) {
+      res.status(400).send("username required!");
+      return; 
+    }
+    let fields = ["username", "password", "name", "email"];
+    for (let key in req.body) {
+      if (!fields.includes(key)) {
+        res.status(400).send("Unknown field: " + key);
+        return;
+      }
+    }
+  let sql = `INSERT INTO users (username, first_name)
+  VALUES ('${req.body.username}',
+  '${req.body.password}',
+  '${req.body.name}',
+  '${req.body.email}');
+  SELECT LAST_INSERT_ID();`;
+console.log(sql);
+
+con.query(sql, function (err, result, fields) {
+  if (err) throw err;
+  console.log(result);
+  let output = {
+    id: result[0].insertId,
+    username: req.body.username,
+    password: req.body.password,
+    name: req.body.name,
+    email: req.body.email,
+  };
+  res.send(output);
+});
+});
